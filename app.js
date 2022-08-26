b80c9a246d728260f4e91f2638055a725595532a
あなたの名前 <>
2022-08-26 13:37:47 +0900

    コード整理

--- app.js
+++ app.js
@@ -8,6 +8,11 @@ const computerChoice = document.getElementById("computerChoice");
 const paper = document.getElementById("paper");
 var choices = document.querySelectorAll(".optionContainer>div");
 
+intro_start.addEventListener('click',function(){
+  intro.classList.add('none');
+  game.classList.remove('none');
+});
+
 var playerPoints = 0;
 var computerPoints = 0;
 
@@ -15,10 +20,8 @@ choices.forEach(choice =>{
   choice.addEventListener('click',function(){
     playerChoice.src=this.getAttribute("data-choice");
     var computerNumber =  Math.ceil(Math.random() * 3);
-    
-    // console.log(number);
+
     if(computerNumber==1){
-      // console.log('ok');
       computerChoice.src="./assets/rock.png";
     } else if(computerNumber==2) {
       computerChoice.src="./assets/paper.png";
@@ -27,31 +30,16 @@ choices.forEach(choice =>{
     }
     var playerNumber = this.getAttribute("data-number");
     // playerNumber = playerNumber + 0;
+
     playerNumber = Number(playerNumber);//数字にする
-    // console.log(playerNumber);
     if((playerNumber-1)%3 == computerNumber%3){//playerの勝ち
-      playerPoints = playerPoints+1;
+      playerPoints += 1;
     }else if((playerNumber+1)%3 == computerNumber%3){
-      computerPoints = computerPoints+1;
+      computerPoints += 1;
     }
-    var array1 = [playerPoints,computerPoints];
-    var array2 = [playerNumber,computerNumber];
     player_point.textContent = playerPoints;
     computer_point.textContent = computerPoints;
-    console.log(array2);
-    // console.log((playerNumber-1)%3);
   })
 });
 
-
-// paper.addEventListener('click',function(){
-//   playerChoice.src=this.getAttribute("data-choice");
-//   // console.log('OK');
-// });
-
-
-intro_start.addEventListener('click',function(){
-  intro.classList.add('none');
-  game.classList.remove('none');
-});
 
